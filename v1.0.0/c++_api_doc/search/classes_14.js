b79b1e0f5b9fdbc7f002bb98cbca5a2a885aee90
Thomas Seidel <thomas.seidel@univie.ac.at>
2023-09-20 03:04:42 +0200

    Documentation update

--- v1.0.0/c++_api_doc/search/classes_14.js
+++ v1.0.0/c++_api_doc/search/classes_14.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['uffatomtypepropertytable_7972',['UFFAtomTypePropertyTable',['../classCDPL_1_1ForceField_1_1UFFAtomTypePropertyTable.html',1,'CDPL::ForceField']]],
-  ['unitlower_7973',['UnitLower',['../structCDPL_1_1Math_1_1UnitLower.html',1,'CDPL::Math']]],
-  ['unitupper_7974',['UnitUpper',['../structCDPL_1_1Math_1_1UnitUpper.html',1,'CDPL::Math']]],
-  ['unitvector_7975',['UnitVector',['../classCDPL_1_1Math_1_1UnitVector.html',1,'CDPL::Math']]],
-  ['upper_7976',['Upper',['../structCDPL_1_1Math_1_1Upper.html',1,'CDPL::Math']]]
+  ['uffatomtypepropertytable_7973',['UFFAtomTypePropertyTable',['../classCDPL_1_1ForceField_1_1UFFAtomTypePropertyTable.html',1,'CDPL::ForceField']]],
+  ['unitlower_7974',['UnitLower',['../structCDPL_1_1Math_1_1UnitLower.html',1,'CDPL::Math']]],
+  ['unitupper_7975',['UnitUpper',['../structCDPL_1_1Math_1_1UnitUpper.html',1,'CDPL::Math']]],
+  ['unitvector_7976',['UnitVector',['../classCDPL_1_1Math_1_1UnitVector.html',1,'CDPL::Math']]],
+  ['upper_7977',['Upper',['../structCDPL_1_1Math_1_1Upper.html',1,'CDPL::Math']]]
 ];
--- v1.0.0/c++_api_doc/search/classes_17.js
+++ v1.0.0/c++_api_doc/search/classes_17.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['zerogrid_8082',['ZeroGrid',['../classCDPL_1_1Math_1_1ZeroGrid.html',1,'CDPL::Math']]],
-  ['zeromatrix_8083',['ZeroMatrix',['../classCDPL_1_1Math_1_1ZeroMatrix.html',1,'CDPL::Math']]],
-  ['zerovector_8084',['ZeroVector',['../classCDPL_1_1Math_1_1ZeroVector.html',1,'CDPL::Math']]]
+  ['zerogrid_8083',['ZeroGrid',['../classCDPL_1_1Math_1_1ZeroGrid.html',1,'CDPL::Math']]],
+  ['zeromatrix_8084',['ZeroMatrix',['../classCDPL_1_1Math_1_1ZeroMatrix.html',1,'CDPL::Math']]],
+  ['zerovector_8085',['ZeroVector',['../classCDPL_1_1Math_1_1ZeroVector.html',1,'CDPL::Math']]]
 ];
