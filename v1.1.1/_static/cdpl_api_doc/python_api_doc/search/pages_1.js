ee874a49a23080caac1607b3c44fada543b4a7c8
Thomas Seidel <thomas.seidel@univie.ac.at>
2024-06-26 08:20:35 +0200

    Documentation update

--- v1.1.1/_static/cdpl_api_doc/python_api_doc/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['references_20and_20further_20reading_10019',['References and Further Reading',['../References.html',1,'']]]
-];
--- v1.1.1/_static/cdpl_api_doc/python_api_doc/search/properties_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['remresonanceduplicates_10016',['remResonanceDuplicates',['../classCDPL_1_1Chem_1_1TautomerGenerator.html#acfef972ca90da7277bdcb58e96c4c599',1,'CDPL::Chem::TautomerGenerator']]]
-];
