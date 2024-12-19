3e29d6de66ea854dfcec943cd007420d806ab954
Thomas Seidel <thomas.seidel@univie.ac.at>
2024-12-19 07:24:25 +0100

    Documentation update

--- master/cdpl_api_doc/python_api_doc/search/classes_d.js
+++ master/cdpl_api_doc/python_api_doc/search/classes_d.js
@@ -1,15 +1,15 @@
 var searchData=
 [
-  ['negionizablefeaturegenerator_6332',['NegIonizableFeatureGenerator',['../classCDPL_1_1Pharm_1_1NegIonizableFeatureGenerator.html',1,'CDPL::Pharm']]],
-  ['nitroacitautomerization_6333',['NitroAciTautomerization',['../classCDPL_1_1Chem_1_1NitroAciTautomerization.html',1,'CDPL::Chem']]],
-  ['nitrogenenumerationmode_6334',['NitrogenEnumerationMode',['../classCDPL_1_1ConfGen_1_1NitrogenEnumerationMode.html',1,'CDPL::ConfGen']]],
-  ['nitrosooximetautomerization_6335',['NitrosoOximeTautomerization',['../classCDPL_1_1Chem_1_1NitrosoOximeTautomerization.html',1,'CDPL::Chem']]],
-  ['notatommatchexpression_6336',['NOTAtomMatchExpression',['../classCDPL_1_1Chem_1_1NOTAtomMatchExpression.html',1,'CDPL::Chem']]],
-  ['notbondmatchexpression_6337',['NOTBondMatchExpression',['../classCDPL_1_1Chem_1_1NOTBondMatchExpression.html',1,'CDPL::Chem']]],
-  ['notmoleculargraphmatchexpression_6338',['NOTMolecularGraphMatchExpression',['../classCDPL_1_1Chem_1_1NOTMolecularGraphMatchExpression.html',1,'CDPL::Chem']]],
-  ['notreactionmatchexpression_6339',['NOTReactionMatchExpression',['../classCDPL_1_1Chem_1_1NOTReactionMatchExpression.html',1,'CDPL::Chem']]],
-  ['npoint2dpharmacophorefingerprintgenerator_6340',['NPoint2DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint2DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
-  ['npoint3dpharmacophorefingerprintgenerator_6341',['NPoint3DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint3DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
-  ['npointpharmacophorefingerprintgenerator_6342',['NPointPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPointPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
-  ['nullpointerexception_6343',['NullPointerException',['../classCDPL_1_1Base_1_1NullPointerException.html',1,'CDPL::Base']]]
+  ['negionizablefeaturegenerator_6333',['NegIonizableFeatureGenerator',['../classCDPL_1_1Pharm_1_1NegIonizableFeatureGenerator.html',1,'CDPL::Pharm']]],
+  ['nitroacitautomerization_6334',['NitroAciTautomerization',['../classCDPL_1_1Chem_1_1NitroAciTautomerization.html',1,'CDPL::Chem']]],
+  ['nitrogenenumerationmode_6335',['NitrogenEnumerationMode',['../classCDPL_1_1ConfGen_1_1NitrogenEnumerationMode.html',1,'CDPL::ConfGen']]],
+  ['nitrosooximetautomerization_6336',['NitrosoOximeTautomerization',['../classCDPL_1_1Chem_1_1NitrosoOximeTautomerization.html',1,'CDPL::Chem']]],
+  ['notatommatchexpression_6337',['NOTAtomMatchExpression',['../classCDPL_1_1Chem_1_1NOTAtomMatchExpression.html',1,'CDPL::Chem']]],
+  ['notbondmatchexpression_6338',['NOTBondMatchExpression',['../classCDPL_1_1Chem_1_1NOTBondMatchExpression.html',1,'CDPL::Chem']]],
+  ['notmoleculargraphmatchexpression_6339',['NOTMolecularGraphMatchExpression',['../classCDPL_1_1Chem_1_1NOTMolecularGraphMatchExpression.html',1,'CDPL::Chem']]],
+  ['notreactionmatchexpression_6340',['NOTReactionMatchExpression',['../classCDPL_1_1Chem_1_1NOTReactionMatchExpression.html',1,'CDPL::Chem']]],
+  ['npoint2dpharmacophorefingerprintgenerator_6341',['NPoint2DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint2DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
+  ['npoint3dpharmacophorefingerprintgenerator_6342',['NPoint3DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint3DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
+  ['npointpharmacophorefingerprintgenerator_6343',['NPointPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPointPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
+  ['nullpointerexception_6344',['NullPointerException',['../classCDPL_1_1Base_1_1NullPointerException.html',1,'CDPL::Base']]]
 ];
--- master/cdpl_api_doc/python_api_doc/search/pages_0.js
+++ master/cdpl_api_doc/python_api_doc/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['gnu_20free_20documentation_20license_20v1_2e2_10878',['GNU Free Documentation License V1.2',['../GFDL.html',1,'']]],
-  ['gnu_20lesser_20general_20public_20license_20v2_2e1_10879',['GNU Lesser General Public License V2.1',['../LGPL.html',1,'']]]
+  ['gnu_20free_20documentation_20license_20v1_2e2_10880',['GNU Free Documentation License V1.2',['../GFDL.html',1,'']]],
+  ['gnu_20lesser_20general_20public_20license_20v2_2e1_10881',['GNU Lesser General Public License V2.1',['../LGPL.html',1,'']]]
 ];
