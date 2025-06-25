7d96e0b42deb430933b9c8762762b2f2cf1d456a
Thomas Seidel <thomas.seidel@univie.ac.at>
2025-06-25 23:33:51 +0200

    Documentation update

--- master/cdpl_api_doc/c++_api_doc/search/related_6.js
+++ master/cdpl_api_doc/c++_api_doc/search/related_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['hierarchyview_15947',['HierarchyView',['../classCDPL_1_1Biomol_1_1HierarchyViewModel.html#a776acadd286ce1bcda89504189eb3134',1,'CDPL::Biomol::HierarchyViewModel']]],
-  ['hierarchyviewchain_15948',['HierarchyViewChain',['../classCDPL_1_1Biomol_1_1HierarchyViewFragment.html#a68fb8baeee331ffd66bc400f40934c17',1,'CDPL::Biomol::HierarchyViewFragment']]],
-  ['hierarchyviewmodel_15949',['HierarchyViewModel',['../classCDPL_1_1Biomol_1_1HierarchyViewChain.html#a6b117ebc2016ebefc83fb13bf1a77f66',1,'CDPL::Biomol::HierarchyViewChain']]]
+  ['hierarchyview_15946',['HierarchyView',['../classCDPL_1_1Biomol_1_1HierarchyViewModel.html#a776acadd286ce1bcda89504189eb3134',1,'CDPL::Biomol::HierarchyViewModel']]],
+  ['hierarchyviewchain_15947',['HierarchyViewChain',['../classCDPL_1_1Biomol_1_1HierarchyViewFragment.html#a68fb8baeee331ffd66bc400f40934c17',1,'CDPL::Biomol::HierarchyViewFragment']]],
+  ['hierarchyviewmodel_15948',['HierarchyViewModel',['../classCDPL_1_1Biomol_1_1HierarchyViewChain.html#a6b117ebc2016ebefc83fb13bf1a77f66',1,'CDPL::Biomol::HierarchyViewChain']]]
 ];
