fd6fa8a16bfe6d32687b1fda1251710abcac7137
Thomas Seidel <thomas.seidel@univie.ac.at>
2026-03-02 23:34:00 +0100

    Documentation update

--- master/cdpl_api_doc/python_api_doc/search/classes_9.js
+++ master/cdpl_api_doc/python_api_doc/search/classes_9.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['jmemoleculargraphoutputhandler_6377',['JMEMolecularGraphOutputHandler',['../classCDPL_1_1Chem_1_1JMEMolecularGraphOutputHandler.html',1,'CDPL::Chem']]],
-  ['jmemoleculargraphwriter_6378',['JMEMolecularGraphWriter',['../classCDPL_1_1Chem_1_1JMEMolecularGraphWriter.html',1,'CDPL::Chem']]],
-  ['jmemoleculeinputhandler_6379',['JMEMoleculeInputHandler',['../classCDPL_1_1Chem_1_1JMEMoleculeInputHandler.html',1,'CDPL::Chem']]],
-  ['jmemoleculereader_6380',['JMEMoleculeReader',['../classCDPL_1_1Chem_1_1JMEMoleculeReader.html',1,'CDPL::Chem']]],
-  ['jmereactioninputhandler_6381',['JMEReactionInputHandler',['../classCDPL_1_1Chem_1_1JMEReactionInputHandler.html',1,'CDPL::Chem']]],
-  ['jmereactionoutputhandler_6382',['JMEReactionOutputHandler',['../classCDPL_1_1Chem_1_1JMEReactionOutputHandler.html',1,'CDPL::Chem']]],
-  ['jmereactionreader_6383',['JMEReactionReader',['../classCDPL_1_1Chem_1_1JMEReactionReader.html',1,'CDPL::Chem']]],
-  ['jmereactionwriter_6384',['JMEReactionWriter',['../classCDPL_1_1Chem_1_1JMEReactionWriter.html',1,'CDPL::Chem']]],
-  ['joinstyle_6385',['JoinStyle',['../classCDPL_1_1Vis_1_1Pen_1_1JoinStyle.html',1,'CDPL::Vis::Pen']]]
+  ['jmemoleculargraphoutputhandler_6376',['JMEMolecularGraphOutputHandler',['../classCDPL_1_1Chem_1_1JMEMolecularGraphOutputHandler.html',1,'CDPL::Chem']]],
+  ['jmemoleculargraphwriter_6377',['JMEMolecularGraphWriter',['../classCDPL_1_1Chem_1_1JMEMolecularGraphWriter.html',1,'CDPL::Chem']]],
+  ['jmemoleculeinputhandler_6378',['JMEMoleculeInputHandler',['../classCDPL_1_1Chem_1_1JMEMoleculeInputHandler.html',1,'CDPL::Chem']]],
+  ['jmemoleculereader_6379',['JMEMoleculeReader',['../classCDPL_1_1Chem_1_1JMEMoleculeReader.html',1,'CDPL::Chem']]],
+  ['jmereactioninputhandler_6380',['JMEReactionInputHandler',['../classCDPL_1_1Chem_1_1JMEReactionInputHandler.html',1,'CDPL::Chem']]],
+  ['jmereactionoutputhandler_6381',['JMEReactionOutputHandler',['../classCDPL_1_1Chem_1_1JMEReactionOutputHandler.html',1,'CDPL::Chem']]],
+  ['jmereactionreader_6382',['JMEReactionReader',['../classCDPL_1_1Chem_1_1JMEReactionReader.html',1,'CDPL::Chem']]],
+  ['jmereactionwriter_6383',['JMEReactionWriter',['../classCDPL_1_1Chem_1_1JMEReactionWriter.html',1,'CDPL::Chem']]],
+  ['joinstyle_6384',['JoinStyle',['../classCDPL_1_1Vis_1_1Pen_1_1JoinStyle.html',1,'CDPL::Vis::Pen']]]
 ];
--- master/cdpl_api_doc/python_api_doc/search/classes_d.js
+++ master/cdpl_api_doc/python_api_doc/search/classes_d.js
@@ -1,15 +1,15 @@
 var searchData=
 [
-  ['negionizablefeaturegenerator_6579',['NegIonizableFeatureGenerator',['../classCDPL_1_1Pharm_1_1NegIonizableFeatureGenerator.html',1,'CDPL::Pharm']]],
-  ['nitroacitautomerization_6580',['NitroAciTautomerization',['../classCDPL_1_1Chem_1_1NitroAciTautomerization.html',1,'CDPL::Chem']]],
-  ['nitrogenenumerationmode_6581',['NitrogenEnumerationMode',['../classCDPL_1_1ConfGen_1_1NitrogenEnumerationMode.html',1,'CDPL::ConfGen']]],
-  ['nitrosooximetautomerization_6582',['NitrosoOximeTautomerization',['../classCDPL_1_1Chem_1_1NitrosoOximeTautomerization.html',1,'CDPL::Chem']]],
-  ['notatommatchexpression_6583',['NOTAtomMatchExpression',['../classCDPL_1_1Chem_1_1NOTAtomMatchExpression.html',1,'CDPL::Chem']]],
-  ['notbondmatchexpression_6584',['NOTBondMatchExpression',['../classCDPL_1_1Chem_1_1NOTBondMatchExpression.html',1,'CDPL::Chem']]],
-  ['notmoleculargraphmatchexpression_6585',['NOTMolecularGraphMatchExpression',['../classCDPL_1_1Chem_1_1NOTMolecularGraphMatchExpression.html',1,'CDPL::Chem']]],
-  ['notreactionmatchexpression_6586',['NOTReactionMatchExpression',['../classCDPL_1_1Chem_1_1NOTReactionMatchExpression.html',1,'CDPL::Chem']]],
-  ['npoint2dpharmacophorefingerprintgenerator_6587',['NPoint2DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint2DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
-  ['npoint3dpharmacophorefingerprintgenerator_6588',['NPoint3DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint3DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
-  ['npointpharmacophorefingerprintgenerator_6589',['NPointPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPointPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
-  ['nullpointerexception_6590',['NullPointerException',['../classCDPL_1_1Base_1_1NullPointerException.html',1,'CDPL::Base']]]
+  ['negionizablefeaturegenerator_6578',['NegIonizableFeatureGenerator',['../classCDPL_1_1Pharm_1_1NegIonizableFeatureGenerator.html',1,'CDPL::Pharm']]],
+  ['nitroacitautomerization_6579',['NitroAciTautomerization',['../classCDPL_1_1Chem_1_1NitroAciTautomerization.html',1,'CDPL::Chem']]],
+  ['nitrogenenumerationmode_6580',['NitrogenEnumerationMode',['../classCDPL_1_1ConfGen_1_1NitrogenEnumerationMode.html',1,'CDPL::ConfGen']]],
+  ['nitrosooximetautomerization_6581',['NitrosoOximeTautomerization',['../classCDPL_1_1Chem_1_1NitrosoOximeTautomerization.html',1,'CDPL::Chem']]],
+  ['notatommatchexpression_6582',['NOTAtomMatchExpression',['../classCDPL_1_1Chem_1_1NOTAtomMatchExpression.html',1,'CDPL::Chem']]],
+  ['notbondmatchexpression_6583',['NOTBondMatchExpression',['../classCDPL_1_1Chem_1_1NOTBondMatchExpression.html',1,'CDPL::Chem']]],
+  ['notmoleculargraphmatchexpression_6584',['NOTMolecularGraphMatchExpression',['../classCDPL_1_1Chem_1_1NOTMolecularGraphMatchExpression.html',1,'CDPL::Chem']]],
+  ['notreactionmatchexpression_6585',['NOTReactionMatchExpression',['../classCDPL_1_1Chem_1_1NOTReactionMatchExpression.html',1,'CDPL::Chem']]],
+  ['npoint2dpharmacophorefingerprintgenerator_6586',['NPoint2DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint2DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
+  ['npoint3dpharmacophorefingerprintgenerator_6587',['NPoint3DPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPoint3DPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
+  ['npointpharmacophorefingerprintgenerator_6588',['NPointPharmacophoreFingerprintGenerator',['../classCDPL_1_1Descr_1_1NPointPharmacophoreFingerprintGenerator.html',1,'CDPL::Descr']]],
+  ['nullpointerexception_6589',['NullPointerException',['../classCDPL_1_1Base_1_1NullPointerException.html',1,'CDPL::Base']]]
 ];
