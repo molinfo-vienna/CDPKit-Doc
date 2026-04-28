15c755c3bcf96d9b6438af991f993deefdc12753
Thomas Seidel <thomas.seidel@univie.ac.at>
2026-04-28 11:45:18 +0200

    Documentation update

---
+++ _downloads/5c7966eb2aea474f05b4079a524f816f/gen_kuvek_bp_descr.py
@@ -0,0 +1,251 @@
+##
+# gen_kuvek_bp_descr.py 
+#
+# This file is part of the Chemical Data Processing Toolkit
+#
+# Copyright (C) 2003 Thomas Seidel <thomas.seidel@univie.ac.at>
+#
+# Permission to use, copy, modify, and/or distribute this software for any purpose with
+# or without fee is hereby granted.
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
+# TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
+# NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+##
+
+
+import sys
+import argparse
+import re
+
+import CDPL.Chem as Chem
+import CDPL.Biomol as Biomol
+import CDPL.ForceField as ForceField
+import CDPL.Descr as Descr
+import CDPL.Math as Math
+
+
+# reads and preprocesses the specified receptor structure
+def readAndPrepareReceptorStructure(args: argparse.Namespace) -> Chem.Molecule:
+    sup_fmts = [ Chem.DataFormat.MOL2,
+                 Biomol.DataFormat.PDB,
+                 Biomol.DataFormat.MMTF,
+                 Biomol.DataFormat.MMCIF ]
+
+    # create reader for receptor structure (format specified by file extension)
+    reader = Chem.MoleculeReader(args.receptor_file) 
+                           
+    if reader.getDataFormat() not in sup_fmts:   # check if the format is supported by this script
+        sys.exit('Error: receptor input file format not supported')
+    
+    rec_mol = Chem.BasicMolecule()    # create an instance of the default implementation of the
+                                      # Chem.Molecule interface that will store the receptor struct.
+    try:
+        if not reader.read(rec_mol):  # read receptor structure
+            sys.exit('Error: reading receptor structure failed')
+
+    except Exception as e:
+        sys.exit(f'Error: reading receptor structure failed:\n{str(e)}')            
+
+    # preprocess the receptor structure (removal of residues and
+    # calculation of properties required by the pharm. generation procedure)
+    try:
+        rem_atoms = False
+
+        # delete atoms belonging to residues that should be stripped
+        if args.strip_res_list:            
+            atoms_to_rem = Chem.Fragment() # will store the atoms to delete
+            res_to_strip_ptns = { re.compile(tlc, re.IGNORECASE) for tlc in args.strip_res_list }
+
+            # identify and note atoms belonging to the stripped residues
+            for atom in rec_mol.atoms:
+                # compose identifier of the residue the atom belongs to
+                atom_res_id = f'{Biomol.getChainID(atom).upper()}_{Biomol.getResidueCode(atom).upper()}_{Biomol.getResidueSequenceNumber(atom)}'
+
+                # check if atom belongs to an excluded residue
+                for res_ptn in res_to_strip_ptns:
+                    if res_ptn.search(atom_res_id):
+                        atoms_to_rem.addAtom(atom)
+                        break
+
+            if atoms_to_rem.numAtoms > 0:
+                rec_mol -= atoms_to_rem    # delete atoms from the receptor structure
+                rem_atoms = True
+
+                if not args.quiet:
+                    print(f'! Removed {atoms_to_rem.numAtoms} atom(s) from the receptor structure')
+
+        # calculate required receptor structure properties
+        # (force recalculation if already present and atoms were removed)
+        Chem.perceiveSSSR(rec_mol, rem_atoms)
+        Chem.setRingFlags(rec_mol, rem_atoms)
+        Chem.calcImplicitHydrogenCounts(rec_mol, rem_atoms)
+        Chem.perceiveHybridizationStates(rec_mol, rem_atoms)
+        Chem.setAromaticityFlags(rec_mol, rem_atoms)
+
+        if Chem.makeHydrogenComplete(rec_mol):                    # make implicit hydrogens (if any) explicit
+            Chem.calcHydrogen3DCoordinates(rec_mol)               # calculate 3D coordinates for the added expl. hydrogens
+            Biomol.setHydrogenResidueSequenceInfo(rec_mol, False) # set residue information for the added expl. hydrogens
+
+        # calc. MMFF94 partial charges
+        ForceField.perceiveMMFF94AromaticRings(rec_mol, False)        # perceive aromatic rings according to the MMFF94 aroamticity model and store data as Chem.MolecularGraph property
+        ForceField.assignMMFF94AtomTypes(rec_mol, False, False)       # perceive MMFF94 atom types (tolerant mode) set corresponding property for all atoms
+        ForceField.assignMMFF94BondTypeIndices(rec_mol, False, False) # perceive MMFF94 bond types (tolerant mode) set corresponding property for all bonds
+        ForceField.calcMMFF94AtomCharges(rec_mol, False, False)       # calculate MMFF94 atom charges (tolerant mode) set corresponding property for all atoms
+
+    except Exception as e:
+        sys.exit(f'Error: processing of receptor structure failed:\n{str(e)}')            
+
+    return rec_mol
+   
+def parseArgs() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Generates a binding pocket shape/electrostatics descriptor for a given receptor '\
+                                    'structure according to the procedure devised by Kuvek et al.')
+
+    parser.add_argument('-i',
+                        dest='receptor_file',
+                        required=True,
+                        metavar='<file>',
+                        help='Receptor structure input file (*.mol2, *.pdb, *.mmtf, *.cif, *.mmcif)')
+    parser.add_argument('-o',
+                        dest='out_file',
+                        required=True,
+                        metavar='<file>',
+                        help='Descriptor output file')
+    parser.add_argument('-s',
+                        dest='strip_res_list',
+                        required=False,
+                        metavar='<res-id>',
+                        nargs='+',
+                        help='Whitespace separated list of identifiers of residues to remove from the receptor structure (e.g. an existing ligand). '\
+                        'Residue identifiers consist of three components separated by an underscore: [chain id]_[tlc]_[res. seq. no.]. '\
+                        'The individual components are optional and the whole string is interpreted '\
+                        'as a regular expression that gets matched against the residue id of '\
+                        'each receptor atom. Examples: HOH -> rem. all waters, A_MET -> remove all MET residues of chain A, '\
+                        '_300$ -> remove all residues with sequ. number 300')
+    parser.add_argument('-c',
+                        dest='sphere_ctr',
+                        required=True,
+                        metavar='<float>',
+                        nargs=3,
+                        type=float,
+                        help='Whitespace separated list of probe sphere center x, y and z coordinates')
+    parser.add_argument('-r',
+                        dest='sphere_rad',
+                        required=False,
+                        metavar='<float>',
+                        type=float,
+                        default=20.0,
+                        help='Probe sphere radius (default: 20.0)')
+    parser.add_argument('-n',
+                        dest='num_test_vecs',
+                        required=False,
+                        metavar='<integer>',
+                        type=int,
+                        default=492,
+                        help='Number of intersection test vectors (default: 492)')
+    parser.add_argument('-t',
+                        dest='write_test_vecs',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='Output test vector x, y and z coordinates for each descriptor element (default: false)')
+    parser.add_argument('-p',
+                        dest='write_inters_pts',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='Output test vector atom intersection point x, y and z coordinates for each descriptor element (default: false)')
+    parser.add_argument('-x',
+                        dest='test_vec_x_range',
+                        required=False,
+                        metavar='<float>',
+                        nargs=2,
+                        type=float,
+                        help='Allowed test vector x coordinate range. If a test vector is outside of '\
+                        'this range then the associated descr. element will not be output (default: [-1.0, 1.0])')
+    parser.add_argument('-y',
+                        dest='test_vec_y_range',
+                        required=False,
+                        metavar='<float>',
+                        nargs=2,
+                        type=float,
+                        help='Allowed test vector y coordinate range. If a test vector is outside of '\
+                        'this range then the associated descr. element will not be output (default: [-1.0, 1.0])')
+    parser.add_argument('-z',
+                        dest='test_vec_z_range',
+                        required=False,
+                        metavar='<float>',
+                        nargs=2,
+                        type=float,
+                        help='Allowed test vector z coordinate range. If a test vector is outside of '\
+                        'this range then the associated descr. element will not be output (default: [-1.0, 1.0])')
+    parser.add_argument('-q',
+                        dest='quiet',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='Disable progress output (default: false)')
+
+    return parser.parse_args()
+
+def main() -> None:
+    args = parseArgs()
+
+    # read and preprocess the receptor structure
+    rec_mol = readAndPrepareReceptorStructure(args)
+
+    # create and initialize an instance of the class Descr.KuvekPocketDescriptorCalculator which
+    # will perform the descriptor generation
+    descr_calc = Descr.KuvekPocketDescriptorCalculator()
+
+    descr_calc.sphereRadius = args.sphere_rad       # apply the -r argument
+    descr_calc.numTestVectors = args.num_test_vecs  # apply the -n argument
+
+    # modify the atom charge function to retrieve MMFF94 partial charges
+    descr_calc.atomChargeFunction = ForceField.getMMFF94Charge
+
+    # vector that will store the binding pocket descriptor values
+    descr = Math.DVector()
+
+    try:
+        # calculate descriptor
+        descr_calc.calculate(args.sphere_ctr, rec_mol, descr)
+
+        # output descriptor vector elements
+        with open(args.out_file, 'w') as out_file:
+            for i in range(descr_calc.numTestVectors):
+                tv = descr_calc.getTestVector(i)
+
+                if args.test_vec_x_range and (tv(0) < args.test_vec_x_range[0] or tv(0) > args.test_vec_x_range[1]):
+                    continue
+
+                if args.test_vec_y_range and (tv(1) < args.test_vec_y_range[0] or tv(1) > args.test_vec_y_range[1]):
+                    continue
+
+                if args.test_vec_z_range and (tv(2) < args.test_vec_z_range[0] or tv(2) > args.test_vec_z_range[1]):
+                    continue
+                
+                out_file.write(f'{descr(i * 2):.4f} {descr(i * 2 + 1):.4f}')
+                
+                if args.write_test_vecs:
+                    out_file.write(f' {tv(0):.4f} {tv(1):.4f} {tv(2):.4f}')
+
+                if args.write_inters_pts:
+                    ip = tv * descr(i * 2)
+                    out_file.write(f' {ip(0):.4f} {ip(1):.4f} {ip(2):.4f}')
+                    
+                out_file.write('\n')
+         
+    except Exception as e:    # handle exception raised in case of severe errors
+        sys.exit(f'Error: descriptor calculation failed:\n{str(e)}')
+   
+    if not args.quiet:
+        print('Done!')
+
+    sys.exit(0)
+        
+if __name__ == '__main__':
+    main()
---
+++ v1.3.0/_downloads/a912bb4b8236c1ffdea6152e979ab887/edit_mols.py
@@ -0,0 +1,206 @@
+##
+# edit_mols.py 
+#
+# This file is part of the Chemical Data Processing Toolkit
+#
+# Copyright (C) 2003 Thomas Seidel <thomas.seidel@univie.ac.at>
+#
+# Permission to use, copy, modify, and/or distribute this software for any purpose with
+# or without fee is hereby granted.
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
+# TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
+# NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+##
+
+
+import sys
+import argparse
+import pathlib
+
+import CDPL.Chem as Chem
+
+
+# exhaustively edits matching substructures of the argument molecule according to the 
+# specified editing instructions using the provided list of initialized
+# Chem.SubstructureEditor instances
+def editMolecule(mol: Chem.Molecule, ed_list: list, args: argparse.Namespace) -> int:
+    # calculate several required properties
+    Chem.initSubstructureSearchTarget(mol, False)
+
+    h_changes = False
+    
+    if args.rem_h:   # remove ordinary (with standard form. charge, isotope, connectivity) hydrogens, if desired
+        h_changes = Chem.makeOrdinaryHydrogenDeplete(mol, Chem.AtomPropertyFlag.ISOTOPE | Chem.AtomPropertyFlag.FORMAL_CHARGE | Chem.AtomPropertyFlag.EXPLICIT_BOND_COUNT, True)
+    elif args.add_h: # make hydrogen complete, if desired
+        h_changes = Chem.makeHydrogenComplete(mol)
+
+    if h_changes:      # if expl. hydrogen count has changed -> recompute/invalidate dependent properties
+        Chem.clearComponents(mol)
+        Chem.calcAtomStereoDescriptors(mol, True, 0, False)
+        Chem.calcBondStereoDescriptors(mol, True, 0, False)
+ 
+    num_edits = 0
+
+    # perform the editing work via the provided Chem.SubstructureEditor instances
+    for editor in ed_list:
+        num_edits += editor.edit(mol)
+
+    # if structural changes were made clear 2D and 3D atom coordinates since they became invalid
+    if num_edits > 0 or h_changes:
+        Chem.setMDLDimensionality(mol, 0)        # for output in one of the MDL formats indicate that there are no atom coordinates present
+
+        for atom in mol.atoms:
+            Chem.clear2DCoordinates(atom)        # delete 2D coordinates
+            Chem.clear3DCoordinates(atom)        # delete 3D coordinates
+            Chem.clear3DCoordinatesArray(atom)   # delete conformer ensemble coordinates 
+
+    return num_edits
+
+# creates and initializes a Chem.SubstructureEditor instance as specified
+# by the given string of substructure search, exclude (optional) and editing result patterns
+# in the format <#Search Patterns> <Search Pattern SMARTS> ...  <#Exclude Patterns> [<Exclude Pattern SMARTS> ...] <Result Pattern SMILES>
+def createSubstructureEditor(ed_ptns: str) -> Chem.SubstructureEditor:
+    editor = Chem.SubstructureEditor()
+    tokens = ed_ptns.split()
+    i = 1
+
+    for j in range(int(tokens[0])):
+        editor.addSearchPattern(Chem.parseSMARTS(tokens[i]))
+        i += 1
+        
+    for j in range(int(tokens[i])):
+        i += 1
+        editor.addExcludePattern(Chem.parseSMARTS(tokens[i]))
+
+    editor.setResultPattern(Chem.parseSMILES(tokens[i + 1]))
+
+    return editor
+
+# processes the value of the argument -p which is either the path to a file containing multiple lines
+# of substructure editing specifications (one per line) or a string directly providing a complete set
+# of search, exclude (optional) and result patterns (format is outline above)
+def createSubstructureEditors(ed_ptns: str) -> list:
+    if pathlib.Path(ed_ptns).is_file():  # if the argument value is a path to an existing file process it line by line
+        editors = []
+
+        with open(ed_ptns, 'r') as ed_ptns_file:
+            for line in ed_ptns_file.readlines():
+                if line.startswith('#'): # lines starting with '#' ar comment lines
+                    continue
+
+                editors.append(createSubstructureEditor(line))
+
+        return editors
+
+    # at this point the argument value directly specifies a set of search, exclude (optional) and result patterns
+    return [ createSubstructureEditor(ed_ptns) ] 
+
+def parseArgs() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Performs modifications on the molecular graph of molecules read from a \
+    specified file according to a provided set of SMARTS/SMILES pattern-based substructure editing rules.')
+
+    parser.add_argument('-i',
+                        dest='in_file',
+                        required=True,
+                        metavar='<file>',
+                        help='Molecule input file')
+    parser.add_argument('-o',
+                        dest='out_file',
+                        required=True,
+                        metavar='<file>',
+                        help='Edited molecule output file')
+    parser.add_argument('-p',
+                        dest='patterns',
+                        required=True,
+                        metavar='<file/string>',
+                        help='A string specifying search, (optional) exclude and result patterns or path to a file providing these (one set per line)')
+    parser.add_argument('-m',
+                        dest='output_mol',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='Output input molecule before the resulting edited molecule (default: false)')
+    parser.add_argument('-d',
+                        dest='rem_h',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='Remove ordinary explicit hydrogens (default: false)')
+    parser.add_argument('-c',
+                        dest='add_h',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='Saturate free valences with explicit hydrogens (default: false)')
+    parser.add_argument('-q',
+                        dest='quiet',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='Disable progress output (default: false)')
+      
+    return parser.parse_args()
+
+def main() -> None:
+    args = parseArgs()
+
+    # create reader for input molecules (format specified by file extension)
+    reader = Chem.MoleculeReader(args.in_file) 
+
+    # create writer for the generated 3D structures (format specified by file extension)
+    writer = Chem.MolecularGraphWriter(args.out_file) 
+
+    # create the list of one or more initialized Chem.SubstructureEditor instances doing the editing work
+    ed_list = createSubstructureEditors(args.patterns)
+    
+    # create an instance of the default implementation of the Chem.Molecule interface
+    mol = Chem.BasicMolecule()
+    i = 1
+
+    # read and process molecules one after the other until the end of input has been reached
+    try:
+        while reader.read(mol):
+            # compose a simple molecule identifier
+            mol_id = Chem.getName(mol).strip() 
+
+            if mol_id == '':
+                mol_id = '#' + str(i) # fallback if name is empty
+            else:
+                mol_id = f'\'{mol_id}\' (#{i})'
+
+            try:
+                # output original molecule before the editing result
+                if args.output_mol:
+                    Chem.calcBasicProperties(mol, False)
+
+                    if not writer.write(mol):
+                        sys.exit(f'Error: writing molecule {mol_id} failed')
+
+                # modify the input molecule according to the specified editing rules 
+                num_changes = editMolecule(mol, ed_list, args)
+                
+                if not args.quiet:
+                    print(f'- Editing molecule {mol_id}: {num_changes} edit(s)')
+
+                Chem.calcBasicProperties(mol, False)
+                    
+                # output the edited molecule
+                if not writer.write(mol):   
+                    sys.exit(f'Error: writing edited molecule {mol_id} failed')
+                        
+            except Exception as e:
+                sys.exit(f'Error: editing or output of molecule {mol_id} failed:\n{str(e)}')
+
+            i += 1
+
+    except Exception as e: # handle exception raised in case of severe read errors
+        sys.exit(f'Error: reading molecule failed:\n{str(e)}')
+
+    writer.close()
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
